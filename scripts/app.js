05030911a562968dd08ccdf3c5c5e8c7195c1c48
MickaelMayer <mayer.mickaelm@gmail.com>
2025-01-15 00:42:30 +0200

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+// scripts/app.js
+function updateTime() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString('en-US', { hour12: true });
+    document.getElementById('time').textContent = timeString;
+  }
+  
+  // Update every second
+  setInterval(updateTime, 1000);
+  updateTime();
